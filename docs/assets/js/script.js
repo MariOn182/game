3c37d02188bccb6df47450095d676fd418fc0bba
Мария <skfjhs@yandex.ru>
2020-08-03 21:27:50 +0300

    new

--- docs/assets/js/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById('hamburger').addEventListener('click',  () => {
-        document.getElementById('hamburger').classList.toggle('hamburger_closed');
-    });
-});
