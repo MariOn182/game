27f4f87e767fad964760e14b39d7fbf3c146ffcc
ofion <ofion@bk.ru>
2020-08-06 10:19:53 +0300

    fix progressbar

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,26 +1,35 @@
+const life = document.getElementById('life');
+const mana = document.getElementById('mana');
+const exp = document.getElementById('exp');
+const xp = document.getElementById('xp');
 
-let life =document.getElementById("life");
-let mana =document.getElementById("mana");
-let exp =document.getElementById("exp");
-let xp =document.getElementById("xp");
-startProgressBar(life, 900);
-startProgressBar(mana, 1200);
-startProgressBar(exp, 600);
-startProgressBar(xp, 800);
+const new_xp = document.getElementById('new_xp');
 
-function startProgressBar(elem, timer) {
-    let k =  0;
-    let timerId = setInterval(() => {
-        if (k > 99) {
-            clearInterval(timerId);
-        }
-        elem.style.width = `${k++}%`;
-    }, timer);
+animateProgressBar(life, 200);
+animateProgressBar(mana, 100);
+animateProgressBar(exp, 300);
+animateProgressBar(xp, 400);
 
+animateNewXp(new_xp);
+
+function animateNewXp(new_xp) {
+    setInterval(() => {
+        new_xp.classList.remove('hidden');
+        new_xp.classList.add('new-xp-points');
+        setTimeout(() => {
+            new_xp.classList.remove('new-xp-points');
+            new_xp.classList.add('hidden');
+        }, 3000);
+    }, 6000);
 }
 
+function animateProgressBar(elem, timer) {
+    let k = 0,
+        isBack = true,
+        timerId = setInterval(function () {
+            k += isBack ? 1 : -1;
+            if (k > 110 || k < -10) isBack = !isBack;
 
-
-
-
-
+            elem.style.width = `${k}%`;
+        }, timer);
+}
--- front/dist/js/script.js
+++ front/dist/js/script.js
@@ -1,29 +1,35 @@
-let life = document.getElementById("life");
-let mana =document.getElementById("mana");
-let exp =document.getElementById("exp");
-let xp =document.getElementById("xp");
+const life = document.getElementById('life');
+const mana = document.getElementById('mana');
+const exp = document.getElementById('exp');
+const xp = document.getElementById('xp');
+
+const new_xp = document.getElementById('new_xp');
+
 animateProgressBar(life, 200);
 animateProgressBar(mana, 100);
 animateProgressBar(exp, 300);
 animateProgressBar(xp, 400);
-//
-// function startProgressBar(elem, timer) {
-//     let k =  0;
-//     let timerId = setInterval(() => {
-//         if (k > 99) {
-//             clearInterval(timerId);
-//         }
-//         elem.style.width = `${k++}%`;
-//     }, timer);
-//
-// }
-function animateProgressBar(elem, timer) {
-  let k = 0,
-     isBack = true,
-     timerId = setInterval(function () {
-      k += isBack ? 1 : -1;
-      if (k > 110 || k < -10) isBack = !isBack;
 
-   elem.style.width = `${k}%`;
-    }, timer);
+animateNewXp(new_xp);
+
+function animateNewXp(new_xp) {
+    setInterval(() => {
+        new_xp.classList.remove('hidden');
+        new_xp.classList.add('new-xp-points');
+        setTimeout(() => {
+            new_xp.classList.remove('new-xp-points');
+            new_xp.classList.add('hidden');
+        }, 3000);
+    }, 6000);
+}
+
+function animateProgressBar(elem, timer) {
+    let k = 0,
+        isBack = true,
+        timerId = setInterval(function () {
+            k += isBack ? 1 : -1;
+            if (k > 110 || k < -10) isBack = !isBack;
+
+            elem.style.width = `${k}%`;
+        }, timer);
 }
